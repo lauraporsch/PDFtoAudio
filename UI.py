3976154a9c80d55704b490040a11a5b9912c5f60
Laura Porsch <laupor112@gmail.com>
2023-07-16 10:14:41 -0600

    added "pass" if user clicks no in "Save file?"-messagebox or closes filedialog.asksaveasfile, changed colors and positioning of widgets to enhance UX

--- UI.py
+++ UI.py
@@ -1,9 +1,9 @@
 from tkinter import *
 
 TEAL = "#569DAA"
-MINT = "#87CBB9"
 DARKTEAL = "#577D86"
 LIGHTMINT = "#B9EDDD"
+FONT = "Arial"
 
 
 class Window(Tk):
@@ -11,33 +11,28 @@ class Window(Tk):
     def __init__(self):
         super().__init__()
         self.title("Text to Speech Converter")
-        self.config(bg=DARKTEAL)
+        self.config(bg=TEAL)
         width = self.winfo_screenwidth()
         height = self.winfo_screenheight()
         self.geometry("%dx%d" % (width, height))
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(4, weight=1)
 
         self.explanation = Label()
         self.explanation.config(text="Upload a PDF file and automatically convert it to MP3\n\nDepending on the file "
-                                "size this might take a moment.", font=("Georgia", 10, "bold"), fg=DARKTEAL,
-                                bg=LIGHTMINT, pady=50, padx=20)
-        self.explanation.grid(column=1, row=0)
-
-        self.empty_space = Label()
-        self.empty_space.config(text="", bg=DARKTEAL)
-        self.empty_space.grid(column=1, row=1, columnspan=5)
+                                "size this might take a moment.", font=(FONT, 15, "bold"), fg=DARKTEAL,
+                                bg=LIGHTMINT, pady=50, padx=90)
+        self.explanation.grid(column=1, row=1, columnspan=3, pady=(50, 50))
 
         self.name_label = Label()
-        self.name_label.config(text="Save Audio File as", font=("Georgia", 10, "bold"), bg=DARKTEAL, fg=LIGHTMINT,
+        self.name_label.config(text="Save Audio File as", font=(FONT, 15), bg=TEAL, fg=LIGHTMINT,
                                padx=20)
-        self.name_label.grid(column=0, row=2)
+        self.name_label.grid(column=1, row=3)
 
-        self.name_file = Entry(width=20, font=("Georgia", 14))
-        self.name_file.grid(column=1, row=2)
+        self.name_file = Entry(width=20, font=(FONT, 15))
+        self.name_file.insert(0, "Name file here")
+        self.name_file.grid(column=2, row=3)
 
-        self.empty_space = Label()
-        self.empty_space.config(text="", bg=DARKTEAL)
-        self.empty_space.grid(column=1, row=3, columnspan=5)
-
-        self.upload_button = Button(self, text="UPLOAD", highlightthickness=0, height=2, width=10, bg=TEAL,
-                                    fg=MINT, font=("Georgia", 10, "bold"))
-        self.upload_button.grid(column=1, row=4, columnspan=2)
+        self.upload_button = Button(self, text="UPLOAD", highlightthickness=0, height=2, width=10, bg=LIGHTMINT,
+                                    fg=DARKTEAL, font=(FONT, 10, "bold"))
+        self.upload_button.grid(column=3, row=3, padx=(20, 0))
--- main.py
+++ main.py
@@ -11,24 +11,27 @@ def convert_file():
     name to the chosen directory, creates messagebox with info, when done"""
     file = tkinter.filedialog.askopenfilename(filetypes=[('PDF files', '*.pdf')])
     reader = PdfReader(file)
-
     name = window.name_file.get()
-
     text = ""
 
     for page_number in range(0, len(reader.pages)-1):
         page = reader.pages[page_number]
         text += page.extract_text()
 
-    tkinter.messagebox.askquestion(title='Save file?', message=f'Save {name} as mp3 file?')
+    user_answer = tkinter.messagebox.askquestion(title='Save file?', message=f'Save {name} as mp3 file?')
 
-    file_path = tkinter.filedialog.asksaveasfilename(initialfile=name)
+    if user_answer == "yes":
+        file_path = tkinter.filedialog.asksaveasfilename(initialfile=name)
+        if file_path == "":
+            pass
+        else:
+            engine = pyttsx3.init()
+            engine.save_to_file(text, f'{file_path}.mp3')
+            engine.runAndWait()
 
-    engine = pyttsx3.init()
-    engine.save_to_file(text, f'{file_path}.mp3')
-    engine.runAndWait()
-
-    tkinter.messagebox.showinfo(title="Done", message="Your file has been saved")
+            tkinter.messagebox.showinfo(title="Done", message="Your file has been saved")
+    else:
+        pass
 
 
 window = Window()
